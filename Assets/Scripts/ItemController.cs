29bb16f3e2ab046fd6659d65a11dc80382fd9291
GibGOAT <ceesaygibril40@gmail.com>
2026-05-02 16:18:00 -0400

    Tagged up

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
-
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.gameObject.tag = "Test";
+            // Assign tag based on this item's name
+            if (gameObject.name == "FI")
+                collision.gameObject.tag = "FM";
+            else if (gameObject.name == "GI")
+                collision.gameObject.tag = "GM";
+            else if (gameObject.name == "MI")
+                collision.gameObject.tag = "MM";
+
             Destroy(gameObject);
         }
     }
