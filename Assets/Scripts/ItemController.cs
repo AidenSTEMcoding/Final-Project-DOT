bf04de637413e96e373b9e0bc1b132c9e07e2d73
GibGOAT <ceesaygibril40@gmail.com>
2026-05-02 14:55:29 -0400

    Tag change ,singleton loading

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+using UnityEngine;
+
 public class ItemController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
+            collision.gameObject.tag = "Test";
             Destroy(gameObject);
         }
     }
-
 }
