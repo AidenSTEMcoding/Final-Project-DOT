f9661bb965bbf27855e82dab71b49793bf1f6df2
GibGOAT <ceesaygibril40@gmail.com>
2026-05-02 15:47:27 -0400

    FINALLLLY SIngleton

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+using UnityEngine;
 
 public class ItemController : MonoBehaviour
+
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the collided object is the Player
-        Player player = collision.GetComponent<Player>();
-        if (player != null)
+        if (collision.CompareTag("Player"))
         {
-            player.SetTag("Test");
-            Destroy(gameObject);   
+            collision.gameObject.tag = "Test";
+            Destroy(gameObject);
         }
     }
 }
